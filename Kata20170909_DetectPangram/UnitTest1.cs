85e96bc45c55a639e772b35c714cb87af476ca7c
cash <cash@cashwu.com>
2017-09-09 21:59:28 +0800

    input all letters test case

--- Kata20170909_DetectPangram/UnitTest1.cs
+++ Kata20170909_DetectPangram/UnitTest1.cs
@@ -7,8 +7,24 @@ namespace Kata20170909_DetectPangram
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_all_letters_should_return_true()
         {
+            PangramShouldBeTrue("abcdefghijklmnopqrstuvwxyz");
+        }
+
+        private static void PangramShouldBeTrue(string str)
+        {
+            var kata = new Kata();
+            var actual = kata.IsPangram(str);
+            Assert.IsTrue(actual);
+        }
+    }
+
+    public class Kata
+    {
+        public bool IsPangram(string str)
+        {
+            return true;
         }
     }
 }
